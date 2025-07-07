9b7c522b492a5156278cf219d48a4d483ad75ebd
Spidy394 <mondalshubhodeep30924@gmail.com>
2025-07-07 22:18:20 +0530

    add pricing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import BgGradient from "@/components/common/bg-gradient";
 import DemoSection from "@/components/home/demo-section";
 import HeroSection from "@/components/home/hero-section";
-import HowItWorkSection from "@/components/home/how-it-works";
+import HowItWorksSection from "@/components/home/how-it-works";
+import PricingSection from "@/components/home/pricing-section";
 
 export default function Home() {
   return (
@@ -9,10 +10,10 @@ export default function Home() {
       <BgGradient />
       <div className="flex flex-col">
         <HeroSection />
+        <DemoSection />
+        <HowItWorksSection />
+        <PricingSection />
       </div>
-      <DemoSection />
-      <HowItWorkSection />
-      {/* <PricingSection /> */}
       {/* <CTASection /> */}
     </div>
   );
